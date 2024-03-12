72387b37eda6fc5cf5d27341a4dfbae5c618bb70
stefaniabruma <stefania.bruma@gmail.com>
2024-03-12 12:32:28 +0200

    TemaProiect1 Reservation modif

--- FirmaTransport/src/main/java/ro/mpp2024/Model/Reservation.java
+++ FirmaTransport/src/main/java/ro/mpp2024/Model/Reservation.java
@@ -4,16 +4,16 @@ import java.util.Objects;
 
 public class Reservation extends Entity<Long>{
 
-    private long route_id;
+    private Route route;
     private int seat_no;
     private String client_name = "-";
 
-    public long getRoute_id() {
-        return route_id;
+    public Route getRoute() {
+        return route;
     }
 
-    public void setRoute_id(long route_id) {
-        this.route_id = route_id;
+    public void setRoute(Route route) {
+        this.route = route;
     }
 
     public int getSeat_no() {
@@ -38,18 +38,18 @@ public class Reservation extends Entity<Long>{
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Reservation that = (Reservation) o;
-        return route_id == that.route_id && seat_no == that.seat_no && Objects.equals(client_name, that.client_name);
+        return route == that.route && seat_no == that.seat_no && Objects.equals(client_name, that.client_name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), route_id, seat_no, client_name);
+        return Objects.hash(super.hashCode(), route, seat_no, client_name);
     }
 
     @Override
     public String toString() {
         return super.toString() +
-                ", route_id=" + route_id +
+                ", route_id=" + route +
                 ", seat_no=" + seat_no +
                 ", client_name='" + client_name + '\'';
     }
