d9b67cd59666d3c478520c6ce84cef84fc27c417
stefaniabruma <stefania.bruma@gmail.com>
2024-03-12 12:31:18 +0200

    TemaProiect1 repo modif

--- FirmaTransport/src/main/java/ro/mpp2024/Repository/Repository.java
+++ FirmaTransport/src/main/java/ro/mpp2024/Repository/Repository.java
@@ -1,8 +1,10 @@
 package ro.mpp2024.Repository;
 
+import ro.mpp2024.Model.Entity;
+
 import java.util.Collection;
 
-public interface Repository<T, Tid> {
+public interface Repository<T extends Entity<Tid>, Tid>  {
     void add(T elem);
     void delete(T elem);
     void update(T elem, Tid id);
